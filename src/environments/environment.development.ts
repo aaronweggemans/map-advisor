1740e2d9d38b8b08efac64ac77ee912978cac7de
aaronweggemans <aaronweggemans@hotmail.nl>
2025-07-16 18:06:11 +0200

    Applicatie maakt nu gebruik van mocks

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,6 @@
 export const environment = {
   url: 'http://localhost:8080/',
+  locationUrl: 'http://localhost:8080/bzk/locatieserver/search/v3_1',
+  openRouteServiceKey: 'k3ystaath1er',
+  openRouteServiceUrl: 'http://localhost:8080/v2/'
 };
